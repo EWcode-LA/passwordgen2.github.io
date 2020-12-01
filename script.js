f379b6849dcd7b658b96c94f8c940fc11fa063cc
EWcode-LA <seth.miller87@proton.mail>
2020-11-30 19:30:44 -0800

    changes

--- script.js
+++ script.js
@@ -1,66 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Dom elements
-const resultEL = document.getElementById('result');
-const lengthEL = document.getElementById('length');
-const uppercaseEL = document.getElementById('uppercase');
-const lowercaseEL = document.getElementById('lowercase');
-const numbersEL = document.getElementById('numbers');
-const symbolsEL = document.getElementById('symbols');
-const generateEL = document.getElementById('generate');
-const clipboardEL = document.getElementById('clipboard');
-
-
-const randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbols: getRandomSymbol
-};
-
-generateEL.addEventListener('click', () => {
-  const length = lengthEL.value;
-});
-//Generator Functions
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
-}
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() *26) + 65);
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() *10) + 48);
-}
-
-function getRandomSymbol() {
-  const symbols = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-
-
-//console.log(getRandomNumber());
-
-generateBtn.addEventListener("click", function(event) {
-  event.preventDefault();
-  var characters = prompt("Please enter a number from 8 to 128 to generate your secure password!")
-   while(length <=8 || length >=129 ){
-     alert("Choose different characters")
-     prompt("Choose between 8-128");
-   }
-});
-
 // Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//create prompt with instructions after button is clicked
+
+var characters = prompt ("Enter a number between 8 and 128 characters");
+
+//create variables using a for loop and multiple if else statements. 
+ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+ var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ var numChar = "0123456789";
+ var specialCase = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
 
 
+ // create if/else statements
 
-
-
-
-
+ if (characters<8) {
+     alert("Password length must be between 8 and 128 characters");
+ } else if (characters>128){
+     alert("Password length must be between 8 and 128 characters");
+ } 
 
 
 
