c5e406b5b7cdda153a26edeeb15ce34a638a6a6f
EWcode-LA <seth.miller87@proton.mail>
2020-12-28 16:42:54 -0800

    updates

--- script.js
+++ script.js
@@ -18,11 +18,11 @@ generateBtn.addEventListener("click", writePassword);
 var characters = prompt ("Enter a number between 8 and 128 characters");
 
 //create variables using a for loop and multiple if else statements. 
- var lowerCase = "abcdefghijklmnopqrstuvwxyz";
- var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
- var numChar = "0123456789";
- var specialCase = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
-
+ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+ var numChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];;
+ var specialCase = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+ var options;
 
  // create if/else statements
 
@@ -32,5 +32,109 @@ var characters = prompt ("Enter a number between 8 and 128 characters");
      alert("Password length must be between 8 and 128 characters");
  } 
 
+//add function to allow user to choose to add elements of variables to generate password. 
+
+lowerCase = confirm("Do you want lowercase characters in your password?")
+if (lowerCase) {
+  alert("Great")
+}
+else {
+  alert("Ok")
+}
+
+upperCase = confirm("Do you want uppercase characters in your password?")
+if (upperCase) {
+  alert("Great")
+}
+else {
+  alert("Ok")
+}
+
+numChar = confirm("Do you want number characters in your password?")
+if (numChar) {
+  alert("Great")
+}
+else {
+  alert("Ok")
+}
+
+specialCase = confirm("Do you want special case characters in your password?")
+if (specialCase) {
+  alert("Great")
+}
+else {
+  alert("Ok")
+}
+
+if (!lowerCase && !upperCase && !numChar && !specialCase) {
+  return alert("You must select at least one of the criteria. Please try again!")
+}
+
+else if (!lowerCase && !upperCase && !numChar && !specialCase) {
+  options = lowerCase.concat(upperCase, numChar, specialCase);
+}
+
+else if (upperCase && numChar && specialCase) {
+  options = upperCase.concat(numChar, specialCase);
+}
+
+else if (lowerCase && numChar && specialCase) {
+  options = lowerCase.concat(numChar, specialCase);
+}
+
+else if (lowerCase && upperCase && specialCase) {
+  options = lowerCase.concat(upperCase, specialCase);
+}
+
+else if (lowerCase && upperCase && specialCase) {
+  options = lowerCase.concat(upperCase, numChar);
+}
+
+else if (numChar && specialCase) {
+  options = numChar.concat(specialCase);
+}
+
+else if (upperCase && specialCase) {
+  options = upperCase.concat(specialCase);
+}
+
+else if (upperCase && numChar) {
+  options = upperCase.concat(numChar);
+}
+
+else if (lowerCase && specialCase) {
+  options = lowerCase.concat(specialCase);
+}
+
+else if (lowerCase && numChar) {
+  options = lowerCase.concat(numChar);
+}
+
+else if (lowerCase && upperCase) {
+  options = lowerCase.concat(upperCase);
+}
+
+else if (lowerCase) {
+  options = lowerCase;
+}
+
+else if (upperCase) {
+  options = upperCase;
+}
+
+else if (numChar) {
+  options = numChar;
+}
+
+else if (specialCase) {
+  options = specialCase
+}
+
+var results =[]
+
+for(var i = 0; i < length; i++) {
+  var temp = options[Math.floor(Math.random() * options.length)];
+  results.push(temp);
+};
 
 
